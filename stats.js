1c1cc752cc1355bd9fb0a206041f1715898e9f81
sodom1 <3078747855@qq.com>
2026-07-16 23:35:20 +0800

    Add files via upload

--- stats.js
+++ stats.js
@@ -3,8 +3,8 @@ const MAX_EVENTS = 5000;
 
 function getRedisConfig() {
   return {
-    url: (process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL || '').replace(/\/$/, ''),
-    token: process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN || ''
+    url: (process.env.UPSTASH_REDIS_REST_URL || process.env.UPSTASH_REDIS_REST_KV_REST_API_URL || process.env.KV_REST_API_URL || '').replace(/\/$/, ''),
+    token: process.env.UPSTASH_REDIS_REST_TOKEN || process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN || process.env.KV_REST_API_TOKEN || ''
   };
 }
 
--- track.js
+++ track.js
@@ -12,8 +12,8 @@ const ALLOWED_EVENTS = new Set([
 
 function getRedisConfig() {
   return {
-    url: (process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL || '').replace(/\/$/, ''),
-    token: process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN || ''
+    url: (process.env.UPSTASH_REDIS_REST_URL || process.env.UPSTASH_REDIS_REST_KV_REST_API_URL || process.env.KV_REST_API_URL || '').replace(/\/$/, ''),
+    token: process.env.UPSTASH_REDIS_REST_TOKEN || process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN || process.env.KV_REST_API_TOKEN || ''
   };
 }
 
